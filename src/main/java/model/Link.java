d2addd73163272157dc335a01483cd5ea14950de
mike280203 <mikesteidle2802@gmail.com>
2026-04-20 11:52:36 +0200

    Initial Quarkus Linktree setup

---
+++ src/main/java/model/Link.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.List;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "links")
+public class Link extends PanacheEntity {
+
+    @Column(nullable = false, length = 120)
+    public String title;
+
+    @Column(nullable = false, length = 2048)
+    public String url;
+
+    @Column(length = 500)
+    public String description;
+
+    @Column(length = 100)
+    public String icon;
+
+    @Column(nullable = false)
+    public Integer sortOrder = 0;
+
+    @Column(nullable = false)
+    public boolean active = true;
+
+    public static List<Link> findActiveOrdered() {
+        return find("active = true order by sortOrder asc, id asc").list();
+    }
+}
